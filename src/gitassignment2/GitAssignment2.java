37012515e054213a26ec21d0cdb69c43b97cd5a0
S515802 <S515802@NW27749.nwmissouri.edu>
2013-06-04 14:36:22 -0500

    Added a print statement(Santhosh Palla)

---
+++ src/gitassignment2/GitAssignment2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitassignment2;
+
+/**
+ *
+ * @author S515802
+ */
+public class GitAssignment2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("santhosh palla");
+    }
+}
